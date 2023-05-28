a019ed4c92b9c8be173cef674c6e16c7db041b64
SadiyahAfroz <safroz0108@gmail.com>
2023-05-28 16:03:53 +0530

    Added 2 sum solution

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
 #TwoSum 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in nums:
+            a = nums.index(i)
+            for n in range(len(nums)):
+                if n == a:
+                    continue
+                else:
+                    if i + nums[n] ==target:
+                        b = n
+                        return a,n
+               
