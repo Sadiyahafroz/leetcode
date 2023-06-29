cf9310609d6a673d3b920c5375a8f3187203d509
SadiyahAfroz <safroz0108@gmail.com>
2023-06-29 11:47:17 +0530

    added solution to 136. Single Number

---
+++ 136.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        for num in nums:
+            if num in d:
+                d[num]+=1
+            else:
+                d[num]=1
+
+        for key, value in d.items():
+            if value == 1:
+                return key
